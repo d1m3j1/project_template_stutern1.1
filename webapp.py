fc0b8d148c9667c3e7b1d86c38999ec92e929103
Oladimeji Shekoni <dimejishekoni@gmail.com>
2022-06-23 13:55:31 +0100

    fixed bug

--- webapp.py
+++ webapp.py
@@ -26,15 +26,19 @@ def predict():
 		Self_Employed = request.args.get('Self_Employed')
 		Income = request.args.get('Income')
 		state = request.args.get('Property_Area')
-		if (state == 'Abuja') | (state == 'Lagos') | (state == 'Port Harcourt'):
+		if state in ['Abuja','Lagos', 'Port Harcourt']:
 			Property_Area = 'Urban'
-		elif (state == 'Kano') | (state == 'Kwara') | (state == 'Oyo') | (state == 'Ogun') | (state == 'Cross River') | (state['State'] == 'Imo'):
+		elif state in ['Kano','Kwara', 'Oyo' , 'Ogun' , 'Cross River', 'Imo']:
 			Property_Area ='Semiurban'
 		else: 
-			Property_Area ='Rural'
+			Property_Area ='Rural'	
 		Prev_Credit_Dur = request.args.get('Prev_Credit_Dur')
 		Total_Amount_Spent = request.args.get('Total_Amount_Spent')
-		Credit_History = request.args.get('Credit_History')
+		hist = request.args.get('Credit_History')
+		if hist in ['Yes']:
+			Credit_History = 1
+		else : 
+			Credit_History = 0
 		
 		#cast to required datatypes
 		Gender = str(Gender)
@@ -46,7 +50,7 @@ def predict():
 		Property_Area = str(Property_Area)
 		Prev_Credit_Dur = int(Prev_Credit_Dur)
 		Total_Amount_Spent = float(Total_Amount_Spent)
-		Credit_History = int(Credit_History)
+		Credit_History = str(Credit_History)
 
 		#make prediction
 		pred = model.predict([[Gender,Married,Dependents,Graduate,Self_Employed,Income,Property_Area, Prev_Credit_Dur,Total_Amount_Spent,Credit_History]]).tolist()
